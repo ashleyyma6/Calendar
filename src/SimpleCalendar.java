e116dad1e9799bff76dda53b1597fd103916a3a3
Yuehongxiao Ma <euyam458689881@gmail.com>
2018-12-12 20:16:56 -0800

    Add Thread to main method to meet requirement.

--- src/SimpleCalendar.java
+++ src/SimpleCalendar.java
@@ -6,16 +6,20 @@ import java.io.IOException;
  * @author Guohua Jiang
  *
  */
-public class SimpleCalendar
-{
-    public static void main(String[] args) throws IOException 
-    {
+public class SimpleCalendar {
+    public static void main(String[] args) throws IOException {
         CalendarFrame cf = new CalendarFrame(); // create the frame
         cf.setSize(new Dimension(700, 700)); // set the size
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); // get screen size
-        cf.setLocation(dim.width/2-cf.getSize().width/2, dim.height/2-cf.getSize().height/2);   // locate the frame
-        cf.setResizable(true);  // resize
-        cf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close
-        cf.setVisible(true); // visible
+        Thread thread = new Thread() {
+            public void run() {
+                cf.setLocation(dim.width / 2 - cf.getSize().width / 2, dim.height / 2 - cf.getSize().height / 2);   // locate the frame
+                cf.setResizable(true);  // resize
+                cf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close
+                cf.setVisible(true); // visible
+                System.out.println("Test Show Main Thread");
+            }
+        };
+        thread.start();
     }
 }
